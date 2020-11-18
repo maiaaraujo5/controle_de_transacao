7c6458f2055f29f005ca2aa8091a907c2a4c6eca
Lucas <maia.araujo5@gmail.com>
2020-11-18 18:20:35 -0300

    feat: add middleware JWT and log middleware

--- internal/app/controle_de_transacao/fx/module/rest/module.go
+++ internal/app/controle_de_transacao/fx/module/rest/module.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	fxHandler "github.com/maiaaraujo5/controle_de_transacao/internal/app/controle_de_transacao/fx/module/rest/handler"
 	"github.com/maiaaraujo5/controle_de_transacao/internal/app/controle_de_transacao/server/rest/handler"
 	"go.uber.org/fx"
@@ -21,8 +22,10 @@ func Routes(createAccount *handler.CreateAccount, recoverAccount *handler.Recove
 	createTransaction *handler.CreateTransaction) *echo.Echo {
 
 	e := echo.New()
+	e.Use(middleware.Logger())
+
 	e.POST("/accounts", createAccount.Handle)
-	e.GET("/accounts", recoverAccount.Handle)
-	e.POST("/transactions", createTransaction.Handle)
+	e.GET("/accounts", recoverAccount.Handle, middleware.JWT([]byte("key-segura")))
+	e.POST("/transactions", createTransaction.Handle, middleware.JWT([]byte("key-segura")))
 	return e
 }
